dc22cef0a8f2dca982ff4f4a9f6cf3bde61a1aef
Nadim Kobeissi <nadim@nadim.cc>
2011-08-01 13:20:25 -0400

    More code splitting and optimization

--- index.php
+++ index.php
@@ -179,9 +179,9 @@ else {
 			global $install;
 			print('<div id="main">
 				<img src="img/cryptocat.png" alt="cryptocat" class="cryptocat" />
-				<form action="'.$install.'" method="post" class="create" id="welcome">
-					<input type="text" class="name" name="name" id="name" onclick="StuffSelect(\'name\');" value="'.$name.'" maxlength="32" autocomplete="off" />
-					<input type="submit" name="create" class="create" value="enter" id="create" onclick="updateaction();" />
+				<form action="'.$install.'" method="get" class="create" id="welcome">
+					<input type="text" class="name" name="c" id="name" onclick="StuffSelect(\'name\');" value="'.$name.'" maxlength="32" autocomplete="off" />
+					<input type="submit" class="create" value="enter" id="create" />
 					<p id="video">(first time? check out this <a href="info">awesome video!</a>)</p>
 				</form>
 				<table>
@@ -207,43 +207,10 @@ else {
 				</p>
 			</div>
 			<script type="text/javascript">
-				var td1 = $("#td1").html();
-				var td2 = $("#td2").html()
-				var td3 = $("#td3").html()
-				var video = $("#video").html()
-				var bottom = $("#bottom").html()
-				var name = $("#name").val()
-				var create = $("#create").val()
-				var curlang = "english";
-						
-				function updateaction() {
-					$("#name").val(document.getElementById("name").value.toLowerCase());
-					document.getElementById("welcome").action = \''.$install.'\'+\'?c=\'+$("#name").val().toLowerCase();
-				}
-						
-				function translate() {
-					if (curlang == "english") {
-						$("#td1").html("<strong>cryptocat</strong> li permet establir xats encriptada, privada de improvisada converses segures. Fes una ullada a aquest <a href=\"info\">vídeo</a> per obtenir consells sobre com començar!");
-						$("#td2").html("els seus missatges són encriptades abans de sortir del seu ordinador usant un algoritme AES-256 i es verifiquen la integritat. totes les converses estan ben esborrat després de 30 minuts d\'inactivitat.");
-						$("#td3").html(\'cryptocat és totalment compatible amb <a target="_blank" href="https://torproject.org">Tor</a> per anònima al xat. utilitzeu cryptocat amb Tor de forma anònima la màxima confidencialitat.\');
-						$("#video").html("(per primera vegada? fes un cop d\'ull a aquest <a href=\"info\">vídeo impressionant!</a>)");
-						$("#bottom").html(\'<a href="#" id="translate" onclick="translate()">english</a> | <a href="about">sobre</a> | cryptocat és el programari beta en el desenvolupament actiu | <a target="_blank" href="https://twitter.com/cryptocatapp">twitter</a> | <a target="_blank" href="https://github.com/kaepora/cryptocat/">github</a>\');
-						$("#name").val("escrigui el seu nom de xat");
-						$("#create").val("entrar");
-						curlang = "catalan";
-					}
-					else if (curlang == "catalan") {
-						$("#td1").html(td1);
-						$("#td2").html(td2);
-						$("#td3").html(td3);
-						$("#video").html(video);
-						$("#bottom").html(bottom);
-						$("#name").val(name);
-						$("#create").val(create);
-						curlang = "english";
-					}
-				}
-			</script>');
+				var install = "'.$install.'";
+				');
+				include('js/translate.js');
+			print('</script>');
 		}
 		function createchat($name, $setnick) {
 			global $data, $_SESSION;
@@ -337,7 +304,7 @@ else {
 				var update = "'.$update.'";
 				var name = "'.$name.'";
 				var maxinput = "'.$maxinput.'";');
-			include("js/cat.js");
+			include('js/cat.js');
 			print('</script>');
 		}
 		
---
+++ js/translate.js
@@ -0,0 +1,36 @@
+var td1 = $("#td1").html();
+var td2 = $("#td2").html()
+var td3 = $("#td3").html()
+var video = $("#video").html()
+var bottom = $("#bottom").html()
+var name = $("#name").val()
+var create = $("#create").val()
+var curlang = "english";
+		
+function updateaction() {
+	$("#name").val(document.getElementById("name").value.toLowerCase());
+	document.getElementById("welcome").action = install + "?c=" + $("#name").val().toLowerCase();
+}
+		
+function translate() {
+	if (curlang == "english") {
+		$("#td1").html("<strong>cryptocat</strong> li permet establir xats encriptada, privada de improvisada converses segures. Fes una ullada a aquest <a href=\"info\">vídeo</a> per obtenir consells sobre com començar!");
+		$("#td2").html("els seus missatges són encriptades abans de sortir del seu ordinador usant un algoritme AES-256 i es verifiquen la integritat. totes les converses estan ben esborrat després de 30 minuts d'inactivitat.");
+		$("#td3").html('cryptocat és totalment compatible amb <a target="_blank" href="https://torproject.org">Tor</a> per anònima al xat. utilitzeu cryptocat amb Tor de forma anònima la màxima confidencialitat.');
+		$("#video").html("(per primera vegada? fes un cop d\'ull a aquest <a href=\"info\">vídeo impressionant!</a>)");
+		$("#bottom").html('<a href="#" id="translate" onclick="translate()">english</a> | <a href="about">sobre</a> | cryptocat és el programari beta en el desenvolupament actiu | <a target="_blank" href="https://twitter.com/cryptocatapp">twitter</a> | <a target="_blank" href="https://github.com/kaepora/cryptocat/">github</a>');
+		$("#name").val("escrigui el seu nom de xat");
+		$("#create").val("entrar");
+		curlang = "catalan";
+	}
+	else if (curlang == "catalan") {
+		$("#td1").html(td1);
+		$("#td2").html(td2);
+		$("#td3").html(td3);
+		$("#video").html(video);
+		$("#bottom").html(bottom);
+		$("#name").val(name);
+		$("#create").val(create);
+		curlang = "english";
+	}
+}
