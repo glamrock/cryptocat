08b1c0c21636d35add1c0ea31e3a66c6dadf1c03
Nadim Kobeissi <nadim@nadim.cc>
2011-12-21 10:50:28 -0500

    Added Basque translation, generously contributed by Mikel Iturbe Urretxa.

--- js/welcome.js
+++ js/welcome.js
@@ -55,9 +55,9 @@ var lettersonly = "letters and numbers only";
 function translate(language) {
 	if (language == "cat") {
 		$("#td1").html("<strong>Cryptocat</strong> li permet crear instantàniament converses segura i encriptada. Es tracta d'una alternativa de codi obert als serveis invasius, com ara el xat de Facebook.");
-		$("#td2").html("Missatges es xifren a nivell local i són verificades per la integritat. Converses estan esborrat després de una hora d'inactivitat.");
+		$("#td2").html("Els missatges són xifrats dins el seu navegador utilitzant AES-256 i 4096 bits claus asimètriques. Les converses són amb seguretat eliminat després d'una hora d'inactivitat.");
 		$("#td3").html('Cryptocat és totalment compatible amb <a target=\"_blank\" href="https://torproject.org">Tor</a> i també funciona en iPhone, Android i BlackBerry.');
-		$("#lang").html('<a href="#" onclick="translate(\'fra\')">fra</a> <a href="#" onclick="translate(\'eng\')">eng</a>');
+		$("#lang").html('<a href="#" onclick="translate(\'fra\')">fra</a> <a href="#" onclick="translate(\'eng\')">eng</a> <a href="#" onclick="translate(\'baq\')">baq</a>');
 		$("#notetext").html('Cryptocat proporciona un xifratge fort e les comunicacions segures. No obstant això, es no és un reemplaçament de GPG. Pensar de manera responsable si es troba en extrem, situacions potencialment mortals.');
 		$("#understand").val("Jo entenc.");
 		$("#c").val("nom de la xerrada");
@@ -66,14 +66,25 @@ function translate(language) {
 	}
 	else if (language == "fra") {
 		$("#td1").html("<strong>Cryptocat</strong> vous laisse mettre en place des conversations sécurisées. C'est une alternative open source et privée à d'autres services tel que Facebook Chat.");
-		$("#td2").html("Les messages sont cryptés à l'intérieur de votre navigateur et sont vérifiés pour l'intégrité. Toutes conversations sont effacés après une heure d'inactivité.");
+		$("#td2").html("Les messages sont cryptés à l'intérieur de votre navigateur avec AES-256 et des clefs asymétriques 4096-bit. Toutes conversations sont effacés après une heure d'inactivité.");
 		$("#td3").html('Cryptocat est entièrement compatible avec <a target=\"_blank\" href="https://torproject.org">Tor</a> et travaille aussi sur votre iPhone, Android et BlackBerry.');
-		$("#lang").html('<a href="#" onclick="translate(\'eng\')">eng</a> <a href="#" onclick="translate(\'cat\')">cat</a>');
+		$("#lang").html('<a href="#" onclick="translate(\'eng\')">eng</a> <a href="#" onclick="translate(\'cat\')">cat</a> <a href="#" onclick="translate(\'baq\')">baq</a>');
 		$("#notetext").html('Cryptocat fournit des communications fortement cryptés. Cependant, Cryptocat n\'est pas un remplacement à GPG. Pensez responsablement si vous êtes dans des situations extrêmes.');
 		$("#understand").val("Je comprends.");
 		$("#c").val("nom de la conversation");
 		$("#create").val("entrer");
 		lettersonly = "lettres et chiffres seulement";
+	}
+	else if (language == "baq") {
+		$("#td1").html("<strong>Cryptocat</strong>-ek elkarrizketa seguruak momentuan eratzea ahalbidetzen du. Facebook-eko txataren moduko zerbitzuen alternatiba pribatu, zifratu eta kode-irekikoa da.");
+		$("#td2").html("Mezuak dira zifratzen AES-256 eta 4096-bit asimetrikoaren teklak erabiliz. Elkarrizketak era seguruan ezabatzen dira haietan jardunik gabeko ordu bat igaro ondoren.");
+		$("#td3").html('Cryptocat guztiz bateragarria da Tor-ekin eta zure iPhone, Android edo BlackBerry-an ere badabil.');
+		$("#lang").html('<a href="#" onclick="translate(\'fra\')">fra</a> <a href="#" onclick="translate(\'cat\')">cat</a> <a href="#" onclick="translate(\'eng\')">eng</a>');
+		$("#notetext").html('Cryptocat-ek zifraketa sendoa duten komunikazio seguruak hornitzen ditu. Dena den, ez da GPGren ordezkoa. Arduraz pentsatu muturreko edo bizitza arriskupean dagoen egoera batean bazaude.');
+		$("#understand").val("Ulertzen dut.");
+		$("#c").val("izendatu zure elkarrizketa");
+		$("#create").val("sartu");
+		lettersonly = "letrak eta zenbakiak soilik";
 	}
 	else if (language == "eng") {
 		$("#td1").html(td1);
