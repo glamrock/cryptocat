398c218b7aea828a8e583f3deeece691eb73fc7d
Nadim Kobeissi <nadim@nadim.cc>
2012-01-01 18:56:02 +0200

    Small UI fixes

--- js/cat.js
+++ js/cat.js
@@ -516,33 +516,13 @@ function userinfo(n) {
 
 $("#maximize").click(function(){
 	if ($("#maximize").attr("title") == "contract") {
-		$("#main").animate({
-			"min-width": "600px",
-			"min-height": "420px",
-			width: "600px",
-			height: "420px"
-		}, 500 );
-		$("#info").animate({
-			width: "588px",
-		}, 500 );
-		$("#users").animate({
-			width: "525px",
-			"padding-right": "3px"
-		}, 500 );
-		$("#input").animate({
-			width: "508px",
-		}, 500 );
-		$("#talk").animate({
-			width: "67px"
-		}, 500 );
-		$("#inchat").animate({
-			height: "333px",
-			"margin-bottom": "10px"
-		}, 500 );
-		$("#chat").animate({
-			height: "330px"
-		}, 500, function() {
-			document.getElementById("chat").innerHTML = document.getElementById("chat").innerHTML;
+		$("#main").animate({"margin-top": "2%", "min-width": "600px", "min-height": "420px", width: "600px", height: "420px"}, 500 );
+		$("#info").animate({width: "588px"}, 500 );
+		$("#users").animate({width: "525px", "padding-right": "3px"}, 500 );
+		$("#input").animate({width: "508px"}, 500 );
+		$("#talk").animate({width: "67px"}, 500 );
+		$("#inchat").animate({height: "343px", "margin-bottom": "10px"}, 500 );
+		$("#chat").animate({height: "340px"}, 500, function() {
 			scrolldown();
 		});
 		$("#maximize").attr("src", "img/maximize.png");
@@ -550,32 +530,13 @@ $("#maximize").click(function(){
 		document.getElementById("input").focus();
 	}
 	else {
-		$("#main").animate({
-			"min-width": "900px",
-			width: "80%",
-			height: "90%"
-		}, 500 );
-		$("#info").animate({
-			width: "99%"
-		}, 500 );
-		$("#users").animate({
-			width: "92.3%",
-			"padding-right": "20px"
-		}, 500 );
-		$("#input").animate({
-			width: "92.3%"
-		}, 500 );
-		$("#talk").animate({
-			width: "5%"
-		}, 500 );
-		$("#inchat").animate({
-			height: "90%",
-			"margin-bottom": "-30px"
-		}, 500 );
-		$("#chat").animate({
-			height: "88%"
-		}, 500, function() {
-			document.getElementById("chat").innerHTML = document.getElementById("chat").innerHTML;
+		$("#main").animate({"margin-top": "1%", "min-width": "900px", width: "85%", height: "96.5%"}, 500 );
+		$("#info").animate({width: "99%"}, 500 );
+		$("#users").animate({width: "92.3%", "padding-right": "20px"}, 500 );
+		$("#input").animate({width: "92.3%"}, 500 );
+		$("#talk").animate({width: "5%"}, 500 );
+		$("#inchat").animate({height: "93%", "margin-bottom": "-30px"}, 500 );
+		$("#chat").animate({height: "91%"}, 500, function() {
 			scrolldown();
 		});
 		$("#maximize").attr("src", "img/minimize.png");
