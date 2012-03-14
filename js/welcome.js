5e634c8e1bf65b4e0a8c2b9c7cdc4e969c28e27a
Nadim Kobeissi <nadim@nadim.cc>
2012-03-14 12:47:54 -0400

    Added instructional video

--- js/welcome.js
+++ js/welcome.js
@@ -52,6 +52,18 @@ $("#create").click(function() {
 	}
 });
 
+$("#video").click(function(){
+	$("#front").css('background-color', 'rgba(0, 0, 0, 0.8)');
+	$("#note").html('<input type="button" id="close" value="x" /><span id="notetext"><iframe src="https://player.vimeo.com/video/38439169?color=97ceec&amp;autoplay=1" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></span>');
+	$("#front").fadeIn();
+	$("#close").click(function(){
+		$("#front").fadeOut(function(){
+			$("#note").html('');
+			$("#front").css('background-color', 'rgba(0, 0, 0, 0)');
+		});
+	});
+});
+
 var td1 = $("#td1").html();
 var td2 = $("#td2").html();
 var td3 = $("#td3").html();
