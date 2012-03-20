99192c6301aeadf71ea09090f77968e54e3ec6ab
Nadim Kobeissi <nadim@nadim.cc>
2012-03-19 20:51:12 -0400

    Added emoticons

--- js/cat.js
+++ js/cat.js
@@ -144,10 +144,25 @@ function tagify(line) {
 				}
 			}
 			sanitize = sanitize.join("");
-			line = line.replace(sanitize, "<a target=\"_blank\" href=\"" + sanitize + "\">" + match[mc] + "</a>");
+			line = line.replace(sanitize, '<a target="_blank" href="' + sanitize + '">' + match[mc] + '</a>');
 		}
 	}
-	line = line.replace(/\&lt\;3/g, "<span class=\"monospace\">&#9829;</span>");
+	else {
+		line = line.replace(/(:|=)-?3/gi, '<div class="emoticon" id="e_cat"></div> ');
+		line = line.replace(/(:|=)-?'\(/gi, '<div class="emoticon" id="e_cry"></div> ');
+		line = line.replace(/(:|=)-?o/gi, '<div class="emoticon" id="e_gasp"></div> ');
+		line = line.replace(/(:|=)-?D/gi, '<div class="emoticon" id="e_grin"></div> ');
+		line = line.replace(/(:|=)-?\(/gi, '<div class="emoticon" id="e_sad"></div> ');
+		line = line.replace(/(:|=)-?\)/gi, '<div class="emoticon" id="e_smile"></div> ');
+		line = line.replace(/-_-/gi, '<div class="emoticon" id="e_squint"></div> ');
+		line = line.replace(/(:|=)-?p/gi, '<div class="emoticon" id="e_tongue"></div> ');
+		line = line.replace(/(:|=)-?(\/|s)/gi, '<div class="emoticon" id="e_unsure"></div> ');
+		line = line.replace(/;-?\)/gi, '<div class="emoticon" id="e_wink"></div> ');
+		line = line.replace(/;-?\p/gi, '<div class="emoticon" id="e_winktongue"></div> ');
+		line = line.replace(/\^_?\^/gi, '<div class="emoticon" id="e_yay"></div> ');
+		line = line.replace(/(:|=)-?x/gi, '<div class="emoticon" id="e_shut"></div> ');
+		line = line.replace(/\&lt\;3/g, '<span class="monospace">&#9829;</span>');
+	}
 	thisnick = line.match(/^[a-z]{1,12}/).toString();
 	if (match = line.match(/^[a-z]{1,12}/)) {
 		var atmatch = line.match(/^[a-z]{1,12}:\s\@[a-z]{1,12}/);
