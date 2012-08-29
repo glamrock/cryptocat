11276834af6af87e689fc5eec27163dc5f4f9507
Nadim Kobeissi <nadim@nadim.cc>
2012-08-29 19:49:20 -0400

    Removing unused code
    Preparing handlePresence(); to be rewritten from scratch
    Code is now totally unusable; currently in middle of switching to new
    model

--- testing/js/cryptocat.js
+++ testing/js/cryptocat.js
@@ -13,7 +13,7 @@ var currentConversation = 0;
 var audioNotifications = 0;
 var currentStatus = 'online';
 var soundEmbed = null;
-var conn, myID, chatName;
+var conn, chatName, myNickname;
 $('.input[title]').qtip();
 $('.button[title]').qtip();
 
@@ -95,7 +95,7 @@ function conversationSwitch(buddy) {
 	});
 }
 
-// Handles login failures (this function will probably be replaced)
+// Handles login failures
 function loginFail(message) {
 	$('#loginInfo').html(message);
 	$('#bubble').animate({'left': '+=5px'}, 130)
@@ -179,51 +179,30 @@ function randomString(size, alpha, uppercase, numeric) {
 	return result;
 }
 
-// Converts format of jid so that it's usable as an element ID.
-function jid2ID(jid) {
-	jid = jid.match(/^(\w|\@|\.)+/);
-	return jid[0].replace('@', '-').replace(/\./g, '-');
-}
-
-// Simply shortens a buddy's name to an acceptable size for display
-// in certain parts of the UI.
-function shortenBuddy(buddy, length) {
-	if (buddy.length > length) {
-		return buddy.substring(0, (length -2)) + '..';
+// Simply shortens a string `string` to length `length.
+// Adds '..' to delineate that string was shortened.
+function shortenString(string, length) {
+	if (string.length > length) {
+		return string.substring(0, (length -2)) + '..';
 	}
-	return buddy;
+	return string;
 }
 
 // Builds a buddy element to be added to the buddy list.
 function buildBuddy(buddyObject) {
 	if (buddyObject.name.match(/^(\w|\s)+$/)) {
-		var name = shortenBuddy(buddyObject.name, 19);
+		var nick = shortenString(buddyObject.nick, 19);
 	}
 	else {
-		var name = shortenBuddy(buddyObject.jid, 19);
+		var alias = shortenString(buddyObject.alias, 19);
 	}
-	$('<div class="buddy" title="' + buddyObject.jid + '" id="' + jid2ID(buddyObject.jid) + '" status="offline">'
-		+ '<span>' + name + '</span>' + '<div class="buddyMenu" id="' + jid2ID(buddyObject.jid)
-		+ '-menu"></div></div>').insertAfter('#buddiesOffline').slideDown('fast');
-}
-
-// Handle incoming messages from the XMPP server.
-function handleMessage(message) {
-	console.log(message);
-	var from = jid2ID($(message).attr('from'));
-	var rosterID = $(message).attr('from').match(/^(\w|\@|\.)+/)[0];
-	var sender = $('#' + jid2ID(rosterID)).find('span').html().match(/^\w+/)[0];
-	var body = $(message).find('body').text();
-	addtoConversation(body, sender, rosterID);
-	if (currentConversation !== rosterID) {
-		var backgroundColor = $('#' + from).css('background-color');
-		$('#' + from).css('background-image', 'url("img/newMessage.png")');
-		$('#' + from).animate({'backgroundColor': '#A7D8F7'}).animate({'backgroundColor': backgroundColor});
-	}
-	return true;
+	$('<div class="buddy" title="' + buddyObject.jid + '" id="' + buddyObject.nick + '" status="online">'
+		+ '<span>' + alias + '</span>' + '<div class="buddyMenu" id="' + buddyObject.nick
+		+ '-menu"></div></div>').insertAfter('#buddiesOnline').slideDown('fast');
 }
 
 // Add a `message` from `sender` to the `conversation` display and log.
+// Used internally.
 function addtoConversation(message, sender, conversation) {
 	initiateConversation(conversation);
 	if (sender === loginCredentials[0]) {
@@ -248,7 +227,7 @@ function addtoConversation(message, sender, conversation) {
 		}
 	}
 	var timeStamp = '<span class="timeStamp">' + currentTime(0) + '</span>';
-	var sender = '<span class="sender">' + shortenBuddy(sender, 16) + '</span>';
+	var sender = '<span class="sender">' + shortenString(sender, 16) + '</span>';
 	message = '<div class="Line' + lineDecoration + '">' + timeStamp + sender + message + '</div>';
 	conversations[conversation] += message;
 	if (conversation === currentConversation) {
@@ -262,138 +241,51 @@ function addtoConversation(message, sender, conversation) {
 	}
 }
 
+// Handle incoming messages from the XMPP server.
+function handleMessage(message) {
+	console.log(message);
+	var from = $(message).attr('from');
+	var nick = from.match(/\/\w+/)[0].substring(1);
+	var type = $(message).attr('type');
+	var body = $(message).find('body').text();
+	if (type === 'groupchat') {
+		addtoConversation(body, nick, chatName + '@' + conferenceServer);
+	}
+	else if (type === 'chat') {
+		var rosterID = $(message).attr('from').match(/^(\w|\@|\.)+/)[0];
+		addtoConversation(body, sender, rosterID);
+	}
+	if (currentConversation !== rosterID) {
+		var backgroundColor = $('#' + from).css('background-color');
+		$('#' + from).css('background-image', 'url("img/newMessage.png")');
+		$('#' + from).animate({'backgroundColor': '#A7D8F7'}).animate({'backgroundColor': backgroundColor});
+	}
+	return true;
+}
+
 // Handle incoming presence updates from the XMPP server.
 // Large, but fully handles any relevant presence update.
 function handlePresence(presence) {
 	console.log(presence);
-	var from = jid2ID($(presence).attr('from'));
-	var rosterID = $(presence).attr('from').match(/^(\w|\@|\.)+/)[0];
-	if ((from === myID)
-		|| ($(presence).attr('type') === 'unsubscribed')
-		|| ($(presence).attr('type') === 'error')) {
+	var from = $(presence).attr('from');
+	var nickname = from.match(/\/\w+/)[0].substring(1);
+	console.log('from: ' + from + ' | myNickname: ' + myNickname);
+	
+	if (($(presence).attr('type') === 'error')) {
 		return true;
+	}
+	
+	if (nickname === myNickname) {
+		buildBuddy({nick: from, alias: 'Conversation'});
+		sendStatus();
 	}
 	else {
 		sendStatus();
 	}
 	if ($('#' + from).length === 0) {
-		buildBuddy({jid: rosterID, name: ''});
+		buildBuddy({nick: from, alias: ''});
 	}
-	if ($(presence).attr('type') === 'unavailable') {
-		if ($('#' + from).attr('status') !== 'offline') {
-			$('#' + from).attr('status', 'offline');
-			$('#' + from).animate({
-				'color': '#BBB',
-				'backgroundColor': '#222',
-				'borderLeftColor': '#111'
-			});
-			$('#' + from).css('cursor', 'default');
-			$('#' + from).css('background-image', 'none');
-			if (audioNotifications) {
-				playSound('snd/userOffline.webm');
-			}
-			if ($('#' + from).prev().attr('id') !== 'currentConversation') {
-				$('#' + from).slideUp('fast', function() {
-					$(this).insertAfter('#buddiesOffline').slideDown('fast');
-				});
-			}
-		}
-	}
-	else if ($(presence).attr('type') === 'subscribe') {
-		var authorizeForm = '<form id="authorizeForm"><div class="bar">authorize new buddy?</div>'
-			+ '<div class="bar">' + rosterID + '</div>'
-			+ '<div id="yes" class="yes">yes</div><div id="no" class="no">no</div></form>';
-		dialogBox(authorizeForm, 1, function() {
-			conn.addHandler(handlePresence, null, 'presence');
-		});
-		$('#yes').click(function() {
-			conn.roster.authorize(rosterID);
-			conn.roster.subscribe(rosterID);
-			$('#dialogBoxClose').click();
-		});
-		$('#no').click(function() {
-			conn.roster.unauthorize(rosterID);
-			$('#dialogBoxClose').click();
-		});
-		return false;
-	}
-	else {
-		if ($(presence).find('show').text() === '' || $(presence).find('show').text() === 'chat') {
-			if ($('#' + from).attr('status') !== 'online') {
-				var status = 'online';
-				var backgroundColor = '#76BDE5';
-				var placement = '#buddiesOnline';
-				if (audioNotifications) {
-					playSound('snd/userOnline.webm');
-				}
-			}
-		}
-		else if ($('#' + from).attr('status') !== 'away') {
-				var status = 'away';
-				var backgroundColor = '#5588A5';
-				var placement = '#buddiesAway';
-		}
-		$('#' + from).attr('status', status);
-		if ($('#' + from).attr('title') !== currentConversation) {
-			$('#' + from).animate({
-				'color': '#FFF',
-				'backgroundColor': backgroundColor,
-				'borderLeftColor': '#97CEEC'
-			});
-			$('#' + from).slideUp('fast', function() {
-				$(this).insertAfter(placement).slideDown('fast');
-			});
-		}
-		$('.buddyMenu').unbind('click');
-		buildBuddyMenu();
-		$('#' + from).css('cursor', 'pointer');
-		$('#' + from).unbind('click');
-		$('#' + from).click(function() {
-			if ($(this).prev().attr('id') === 'currentConversation') {
-				$('#userInputText').focus();
-				return true;
-			}
-			$(this).css('background-image', 'none');
-			if ($(this).attr('status') !== 'offline') {
-				if (currentConversation) {
-					var oldConversation = '#' + jid2ID(currentConversation);
-					if ($(oldConversation).attr('status') === 'online') {
-						var placement = '#buddiesOnline';
-						var backgroundColor = '#76BDE5';
-					}
-					else if ($(oldConversation).attr('status') === 'away') {
-						var placement = '#buddiesAway';
-						var backgroundColor = '#5588A5';
-					}
-					else {
-						var placement = '#buddiesOffline';
-						var backgroundColor = '#222';
-					}
-					$(oldConversation).slideUp('fast', function() {
-						$(oldConversation).css('background-color', backgroundColor);
-						$(oldConversation).css('border-bottom', 'none');
-						$(oldConversation).insertAfter(placement).slideDown('fast');
-					});
-				}
-				currentConversation = $(this).attr('title');
-				initiateConversation(currentConversation);
-				$('#conversationWindow').html(conversations[currentConversation]);
-				if (($(this).prev().attr('id') === 'buddiesOnline')
-					|| (($(this).prev().attr('id') === 'buddiesAway')
-					&& $('#buddiesOnline').next().attr('id') === 'buddiesAway')) {
-					$(this).insertAfter('#currentConversation');
-					conversationSwitch($(this).attr('id'));
-				}
-				else {
-					$(this).slideUp('fast', function() {
-						$(this).insertAfter('#currentConversation').slideDown('fast', function() {
-							conversationSwitch($(this).attr('id'));
-						});
-					});
-				}
-			}
-		});
-	}
+	
 	return true;
 }
 
@@ -405,15 +297,6 @@ function sendStatus() {
 	else {
 		conn.send($pres());
 	}
-}
-
-// Set a `nickname` for `buddy`.
-function setNickname(buddy, nickname) {
-	conn.sendIQ(
-		$iq({type: 'set'}).c('query', {xmlns: 'jabber:iq:roster'}).c('item', {
-			jid: buddy, name: nickname
-		})
-	);
 }
 
 // Displays a pretty dialog box with `data` as the content HTML.
@@ -490,20 +373,6 @@ function buildBuddyMenu() {
 							}
 						});
 					});
-					$('.removeBuddy').click(function(event) {
-						event.stopPropagation();
-						var buddy = $('.removeBuddy').parent().attr('id');
-						buddy = buddy.substring(0, (buddy.length - 9));
-						conn.roster.unauthorize($('#' + buddy).attr('title'));
-						conn.roster.unsubscribe($('#' + buddy).attr('title'));
-						var iq = $iq({type: 'set'})
-							.c('query', {xmlns: Strophe.NS.ROSTER})
-							.c('item', {jid: $('#' + buddy).attr('title'), subscription: 'remove'});
-						conn.sendIQ(iq);
-						$('#' + buddy).slideUp('fast', function() {
-							$('#' + buddy).remove();
-						});
-					});
 				});
 			});
 		}
@@ -534,27 +403,6 @@ $('#status').click(function() {
 		currentStatus = 'online';
 		sendStatus();
 	}
-});
-
-// Add buddy button (likely to be removed)
-$('#add').click(function() {
-	if ($('#dialogBoxClose').css('display') === 'block') {
-		return false;
-	}
-	var addBuddyForm = '<form id="addBuddyForm"><div class="bar">add new buddy:</div>'
-		+ '<input id="addBuddyJID" class="bar" type="text" value="user@' + domain + '" autocomplete="off"/>'
-		+ '<input class="yes" id="addBuddySubmit" type="submit" value="Send buddy request"/>'
-		+ '</form>';
-	dialogBox(addBuddyForm, 1);
-	$('#addBuddyJID').click(function() {
-		$(this).select();
-	});
-	$('#addBuddyForm').submit(function() {
-		conn.roster.subscribe($('#addBuddyJID').val());
-		$('#dialogBoxClose').click();
-		return false;
-	});
-	$('#addBuddyJID').select();
 });
 
 // Audio notifications button
@@ -631,6 +479,7 @@ function registerXMPPUser(username, password) {
 		}
 		else if (status === Strophe.Status.REGISTERED) {
 			registrationConnection.disconnect();
+			delete registrationConnection;
 			login(loginCredentials[0], loginCredentials[1]);
 			return true;
 		}
@@ -666,23 +515,17 @@ function login(username, password) {
 						var scrollWidth = document.getElementById('buddyList').scrollWidth;
 						$('#buddyList').css('width', (150 + scrollWidth) + 'px');
 					});
-					conn.roster.init(conn);
-					conn.roster.get(function(roster) {
-						for (var i in roster) {
-							if (!roster[i].name) {
-								roster[i].name = '';
-							}
-							buildBuddy(roster[i]);
-						}
-						sendStatus();
-					});
-					myID = jid2ID(loginCredentials[0] + '@' + domain);
-					conn.muc.join(chatName + '@' + conferenceServer, randomString(20, 1, 1, 1), 
+					myNickname = randomString(20, 1, 1, 1);
+					conn.muc.join(chatName + '@' + conferenceServer, myNickname, 
 						function(message) {
-							handleMessage(message);
+							if (handleMessage(message)) {
+								return true;
+							}
 						}, 
 						function(presence) {
-							console.log(presence);
+							if (handlePresence(presence)) {
+								return true;
+							}
 						}
 					);
 				});
@@ -702,7 +545,6 @@ function login(username, password) {
 						$('#buddyList div').remove();
 						$('#conversationWindow').html('');
 						conversations = [];
-						myID = null;
 						loginCredentials = [];
 						$('#chatName').val('');
 						$('#chatName').removeAttr('readonly');
@@ -720,7 +562,7 @@ function login(username, password) {
 			loginFail('Authentication failure.');
 		}
 		else if (status === Strophe.Status.ERROR) {
-			console.log('status ' + status);
+			loginFail('Error ' + status);
 		}
 	});
 }
