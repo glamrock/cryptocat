e06d1eeec6d3591992383d61674b183cbd100d0c
Nadim Kobeissi <nadim@nadim.cc>
2012-09-23 12:13:56 -0400

    Push builds

--- src/common/js/cryptocat.js
+++ src/common/js/cryptocat.js
@@ -27,7 +27,7 @@ if (!groupChat) {
 var language = window.navigator.userLanguage
 	|| window.navigator.language;
 try {
-		language = Language.set(language);
+		language = Language.set(language.toLowerCase());
 	}
 catch(err) {
 	language = Language.set('en');
