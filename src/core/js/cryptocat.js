77009a6051745fea250abbe396f3691b2f2555d0
Nadim Kobeissi <nadim@nadim.cc>
2013-06-04 14:34:14 -0400

    Tidy up dialog boxes with new UI

--- src/core/js/cryptocat.js
+++ src/core/js/cryptocat.js
@@ -41,18 +41,18 @@ var templates = {
 	buddy: '<div class="buddy" title="{{nickname}}" id="buddy-{{nickname}}" status="online">'
 				+ '<span>{{shortNickname}}</span>'
 		+ '<div class="buddyMenu" id="menu-{{nickname}}"></div></div>',
-	infoDialog: '<input type="button" class="bar" value="{{nickname}}" />'
+	infoDialog: '<div class="title">{{nickname}}</div>'
 		+ '<div id="displayInfo">{{otrFingerprint}}<br /><span id="otrFingerprint"></span><br />'
 			+ '<div id="otrColorprint"></div>{{groupFingerprint}}'
 			+ '<br /><span id="multiPartyFingerprint"></span><br />'
 			+ '<div id="multiPartyColorprint"></div><br /></div>',
-	sendFile: '<div class="bar">{{sendEncryptedFile}}</div>'
+	sendFile: '<div class="title">{{sendEncryptedFile}}</div>'
 		+ '<input type="file" id="fileSelector" name="file[]" />'
-		+ '<input type="button" id="fileSelectButton" class="button" value="{{sendEncryptedFile}}" />'
+		+ '<input type="button" id="fileSelectButton" value="{{sendEncryptedFile}}" />'
 		+ '<div id="fileInfoField">{{fileTransferInfo}}</div>',
 	file: '<div class="fileProgressBar" file="{{message}}"><div class="fileProgressBarFill"></div></div>',
 	message: '<div class="line{{lineDecoration}}"><span class="sender" sender="{{sender}}"'
-	 		+ ' timestamp="{{currentTime}}">{{sender}}</span>{{&message}}</div>'
+	 	+ ' timestamp="{{currentTime}}">{{sender}}</span>{{&message}}</div>'
 }
 
 // Set server information to defaults
