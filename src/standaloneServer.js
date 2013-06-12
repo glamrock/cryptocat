6fb6a20d8de087acc37110f4830cb326b8ccb815
Nadim Kobeissi <nadim@nadim.cc>
2013-06-12 15:55:50 -0400

    Organize

--- src/standaloneServer.js
+++ src/standaloneServer.js
@@ -5,7 +5,7 @@ var express = require("express");
 var app = express();
 var port = 1337;
 
-app.use(express.static(path.join(__dirname, "src/core")));
+app.use(express.static(path.join(__dirname, "core")));
 
 app.listen(port);
 console.log("[Cryptocat] listening on port: " + port);
