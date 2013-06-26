203bff42ecaa119b9645db882e0bff59859170fb
Nadim Kobeissi <nadim@nadim.cc>
2013-06-26 12:52:52 -0400

    Fix Travis

--- src/core/js/cryptocat.js
+++ src/core/js/cryptocat.js
@@ -878,7 +878,7 @@ function smpQuestion(nickname, question) {
 }
 
 // Display SMP authentication
-function displayAuth(nickname) {
+/* function displayAuth(nickname) {
 	ensureOTRdialog(nickname, false, function() {
 		var authDialog = Mustache.render(
 			Cryptocat.templates.authDialog,
@@ -897,7 +897,7 @@ function displayAuth(nickname) {
 				otrKeys[nickname].smpSecret(secret, question)
 			})
 	})
-}
+} */
 
 // Bind buddy menus for new buddies. Used internally.
 function bindBuddyMenu(nickname) {
