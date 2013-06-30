7fe56f0ad6a1b481e66c33592d1dabb338b19901
Nadim Kobeissi <nadim@nadim.cc>
2013-06-30 04:20:48 -0400

    Fix inaccurate comment

--- src/core/js/lib/multiParty.js
+++ src/core/js/lib/multiParty.js
@@ -85,8 +85,7 @@ function checkSize(publicKey) {
 	}
 }
 
-// Generate private key (32 byte random number)
-// Represented in decimal
+// Generate private key (64 random bytes)
 multiParty.genPrivateKey = function() {
 	var rand = Cryptocat.randomString(64, 0, 0, 1, 0)
 	myPrivateKey = BigInt.str2bigInt(rand, 16)
