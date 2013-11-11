0d1c1a11467c5ef4f89d6fb2c0e582cff9712bb8
Nadim Kobeissi <nadim@nadim.cc>
2013-11-11 18:34:38 -0500

    Enable indexedDB on the XUL side for Cryptocat for Firefox
    (More stuff coming, this is related to #461)

--- src/firefox/chrome/content/cryptocat.js
+++ src/firefox/chrome/content/cryptocat.js
@@ -1,37 +1,39 @@
 var cryptocat = function() {
-	Components.utils.import("resource://gre/modules/Services.jsm");
-	Components.utils.import('resource://gre/modules/ctypes.jsm');
-	var prefsService = Services.prefs;
-	var cryptocatRandom = Components.utils.import('chrome://cryptocat/content/generateRandomBytes.jsm');
+	Components.utils.import('resource://gre/modules/Services.jsm')
+	Components.utils.import('resource://gre/modules/ctypes.jsm')
+	Components.classes['@mozilla.org/dom/indexeddb/manager;1']
+		.getService(Ci.nsIIndexedDatabaseManager).initWindowless(this);
+	var prefsService = Services.prefs
+	var cryptocatRandom = Components.utils.import('chrome://cryptocat/content/generateRandomBytes.jsm')
 	return {
 		init: function() {
-			var firstRun = prefsService.getBoolPref('extensions.cryptocat.firstRun');
+			var firstRun = prefsService.getBoolPref('extensions.cryptocat.firstRun')
 			if (firstRun) {
-				Application.prefs.setValue('extensions.cryptocat.firstRun', false);
-				var navBar = document.getElementById('nav-bar');
-				var newSet = navBar.currentSet + ',cryptocatToolbarButton';
-				navBar.currentSet = newSet;
-				navBar.setAttribute('currentset', newSet);
-				document.persist('nav-bar', 'currentset'); 
+				Application.prefs.setValue('extensions.cryptocat.firstRun', false)
+				var navBar = document.getElementById('nav-bar')
+				var newSet = navBar.currentSet + ',cryptocatToolbarButton'
+				navBar.currentSet = newSet
+				navBar.setAttribute('currentset', newSet)
+				document.persist('nav-bar', 'currentset') 
 			}
 		},
 		run: function() {
-			gBrowser.selectedTab = gBrowser.addTab('chrome://cryptocat/content/data/index.html');
+			gBrowser.selectedTab = gBrowser.addTab('chrome://cryptocat/content/data/index.html')
 			window.addEventListener('cryptocatGenerateRandomBytes', function(evt) {
 				cryptocat.generateRandomBytes(evt)
-			}, false, true);
+			}, false, true)
 		},
 		generateRandomBytes: function(evt) {
 			try {
-				cryptocatRandom.WeaveCrypto.initNSS(ctypes.libraryName('nss3'));
+				cryptocatRandom.WeaveCrypto.initNSS(ctypes.libraryName('nss3'))
 			}
 			catch(err) {
-				cryptocatRandom.WeaveCrypto.path = Services.dirsvc.get('GreD', Ci.nsIFile);
-				cryptocatRandom.WeaveCrypto.path.append(ctypes.libraryName('nss3'));
-				cryptocatRandom.WeaveCrypto.initNSS(WeaveCrypto.path.path);
+				cryptocatRandom.WeaveCrypto.path = Services.dirsvc.get('GreD', Ci.nsIFile)
+				cryptocatRandom.WeaveCrypto.path.append(ctypes.libraryName('nss3'))
+				cryptocatRandom.WeaveCrypto.initNSS(WeaveCrypto.path.path)
 			}
-			evt.target.setAttribute('randomValues', cryptocatRandom.WeaveCrypto.generateRandomBytes(40));
+			evt.target.setAttribute('randomValues', cryptocatRandom.WeaveCrypto.generateRandomBytes(40))
 		}
-	};
-}();
-window.addEventListener('load', cryptocat.init(), false);
+	}
+}()
+window.addEventListener('load', cryptocat.init(), false)
